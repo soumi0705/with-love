893ff10d7a3f3e55b891e211888bce73bb357ab6
soumi0705 <somida00@gmail.com>
2023-06-06 16:49:17 +0530

    Checking password

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Message({ text }) {
   return (
@@ -10,11 +10,15 @@ function Message({ text }) {
 
 function App() {
   const json = require("./message.json");
+  const [password, setPassword] = useState("");
   const getRandomMessage = () => {
     const randomIndex = Math.floor(Math.random() * json.length);
     return json[randomIndex];
   };
-  return (
+  useEffect(()=>{
+    console.log(password, process.env.REACT_APP_PASSWORD);
+  })
+  return password === process.env.REACT_APP_PASSWORD ? (
     <div className="wrapper min-h-screen flex text-left justify-center">
       <div className="container mx-auto p-4">
         <div style={{ position: "relative" }}>
@@ -22,9 +26,15 @@ function App() {
             MY SNACK 💗!!!!
           </h1>
         </div>
-
         <Message text={getRandomMessage()} />
       </div>
+    </div>
+  ) : (
+    <div className="wrapper min-h-screen flex text-left justify-center">
+      <label className="text-2xl flex justify-center text-white items-center">
+        Enter password:
+        <input type="text" className="m-2 text-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(pass) => setPassword(pass.target.value)} />
+      </label>
     </div>
   );
 }
