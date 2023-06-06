99b4dd9b44c8f0f8cd04bfdc0644b6ede04d9074
soumi0705 <somida00@gmail.com>
2023-06-06 15:41:23 +0530

    Completed App Development

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Message({ text }) {
+  return (
+    <div className="p-4 my-4 bg-[#ffffffbd] rounded-lg shadow-md text-gray-800">
+      {text}
+    </div>
+  );
+}
 
 function App() {
+  const json = require("./message.json");
+  const getRandomMessage = () => {
+    const randomIndex = Math.floor(Math.random() * json.length);
+    return json[randomIndex];
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper min-h-screen flex text-left justify-center">
+      <div className="container mx-auto p-4">
+        <div style={{ position: "relative" }}>
+          <h1 className="glow font-bold mb-4 text-[#e6e6e6]">
+            MY SNACK 💗!!!!
+          </h1>
+        </div>
+
+        <Message text={getRandomMessage()} />
+      </div>
     </div>
   );
 }
